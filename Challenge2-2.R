8c0cc296dc0ec07b17f2a72c0c3bbe4e7c09452a
nijatgasimovv <74681174+nijatgasimovv@users.noreply.github.com>
2020-12-05 12:28:00 +0100

    Challenge 4

--- Challenge2-2.R
+++ Challenge2-2.R
@@ -8,16 +8,64 @@ library(xopen)     # Quickly opening URLs
 library(jsonlite)  # converts JSON files to R objects
 library(glue)      # concatenate strings
 library(stringi)   # character string/text processing
+library(RSQLite)   # working with databases
 
-# 2. Collect product families
+# 2. Open category page 
 
-url_homepage <- "https://www.rosebikes.de/"
+# Arbitrarily chosen E-bike family, Mountainbike category
+
+url_homepage <- "https://www.radon-bikes.de/e-bike/mountainbike/bikegrid/"
 #xopen(url_homepage)
 html_homepage <- read_html(url_homepage)
 
-rosebike_family <- html_homepage %>%
-  
-  #Get nodes for the families
-  html_nodes(css = ":nth-child(10) .catalog-categories-item__title , :nth-child(9) .catalog-categories-item__title, :nth-child(8) .catalog-categories-item__title, :nth-child(7) .catalog-categories-item__title, :nth-child(6) .catalog-categories-item__title, :nth-child(5) .catalog-categories-item__title, :nth-child(4) .catalog-categories-item__title, .mediumlarge-4:nth-child(3) .catalog-categories-item__title, .mediumlarge-4:nth-child(2) .catalog-categories-item__title, .mediumlarge-4:nth-child(1) .catalog-categories-item__title")
+
+# 3. Scraping product ID's and prices
+
+#Extract the product ID names
+product_id_tbl <- html_homepage %>%
+  #Get the nodes for the product names ...
+  html_nodes(css = ".m-bikegrid__info .a-heading--small") %>%
+  # ...and extract the text of the html attribute
+  html_text() %>%
+  # Delete the unnecessary formatting
+  #str_replace_all("[\t\n\r\v\f]" , "") %>%
+  # Delete whitespace
+  trimws("both") %>%
+  # Convert vector to tibble
+  enframe(name = "position", value = "product_id")
+
+#Extract the product prices
+product_prices_tbl <- html_homepage %>%
+  #Get the nodes for the product prices
+  html_nodes(css = ".currency_eur> .m-bikegrid__price--active") %>%
+  # ...and extract the information of the span attribute
+  html_text("span") %>%
+  # Convert vector to tibble
+  enframe(name = "position", value = "product_prices")
+
+#Bind two columns
+mountainbike_list_tbl <- product_id_tbl %>%
+  bind_cols(product_prices_tbl %>% select("product_prices"))
+
+# 4. Creating database
+
+con <- RSQLite::dbConnect(drv    = SQLite(), 
+                           dbname = "data-science/00_data/Mountain_bikes.sqlite")
+
+dbWriteTable(con, "product_id", product_id_tbl, overwrite=TRUE)
+dbWriteTable(con, "product_prices", product_prices_tbl, overwrite=TRUE)
+
+# 5. Call database
+
+# Return the name of tables
+dbListTables(con)
+
+tbl(con, "product_id")
+
+tbl(con, "product_prices")
+
+
+dbDisconnect(con)
+con
 
 
